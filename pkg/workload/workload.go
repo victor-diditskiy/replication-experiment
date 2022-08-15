68cf6839998b0a721e062baaa07e307d9267b8ca
Victor Diditskii <diditskiy.victor@gmail.com>
2022-08-15 10:54:02 +0300

    R-4: add workload names and workload initialization

--- pkg/workload/workload.go
+++ pkg/workload/workload.go
@@ -4,25 +4,41 @@ import (
 	"context"
 
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	"github.com/victor_diditskiy/replication_experiment/pkg/storage"
 )
+
+const (
+	ReadWorkloadName   Name = "read"
+	InsertWorkloadName Name = "insert"
+	UpdateWorkloadName Name = "update"
+)
+
+type Name string
 
 type Workload interface {
 	Start(ctx context.Context, conf Config) error
 	Stop(conf Config) error
 }
 
+type Workloads map[Name]Workload
+
+func NewWorkloads(log logrus.FieldLogger, storage storage.CombinedStorage) Workloads {
+	return Workloads{
+		ReadWorkloadName:   NewReadWorkload(log, storage),
+		InsertWorkloadName: NewInsertWorkload(log, storage),
+		UpdateWorkloadName: NewUpdateWorkload(log, storage),
+	}
+}
+
 type Config struct {
 	ScaleFactor int
-	MaxEntries  int64
 }
 
 func (c *Config) Validate() error {
 	if c.ScaleFactor < 1 {
 		return errors.New("scale factor can not be less then 1")
-	}
-
-	if c.MaxEntries < 1 {
-		return errors.New("max entries can not be less then 1")
 	}
 
 	return nil
