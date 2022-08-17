5ed95107c21fec237ea95f0195bf0b2685afed61
Victor Diditskii <diditskiy.victor@gmail.com>
2022-08-17 20:57:39 +0300

    R-5: set up taking request metrics

--- pkg/storage/metric.go
+++ pkg/storage/metric.go
@@ -1,1 +1,70 @@
 package storage
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/victor_diditskiy/replication_experiment/pkg/entity"
+	"github.com/victor_diditskiy/replication_experiment/pkg/metric"
+)
+
+type MetricStorage struct {
+	storage CombinedStorage
+}
+
+func NewMetricStorage(storage CombinedStorage) *MetricStorage {
+	return &MetricStorage{
+		storage: storage,
+	}
+}
+
+func (s *MetricStorage) Insert(data entity.Data) error {
+	t := time.Now()
+	defer metric.RequestCounter.With(prometheus.Labels{metric.TypeLabel: metric.WriteType, metric.OperationLabel: metric.InsertOperation}).Inc()
+	defer func() {
+		metric.RequestTimingCounter.
+			With(prometheus.Labels{metric.TypeLabel: metric.WriteType, metric.OperationLabel: metric.InsertOperation}).
+			Observe(float64(time.Since(t).Milliseconds()))
+	}()
+
+	return s.storage.Insert(data)
+}
+
+func (s *MetricStorage) Update(data entity.Data) error {
+	t := time.Now()
+	defer metric.RequestCounter.With(prometheus.Labels{metric.TypeLabel: metric.WriteType, metric.OperationLabel: metric.UpdateOperation}).Inc()
+	defer func() {
+		metric.RequestTimingCounter.
+			With(prometheus.Labels{metric.TypeLabel: metric.WriteType, metric.OperationLabel: metric.UpdateOperation}).
+			Observe(float64(time.Since(t).Milliseconds()))
+	}()
+
+	return s.storage.Update(data)
+}
+
+func (s *MetricStorage) Get(id int64) (*entity.Data, error) {
+	t := time.Now()
+	defer metric.RequestCounter.With(prometheus.Labels{metric.TypeLabel: metric.ReadType, metric.OperationLabel: metric.GetLOperation}).Inc()
+	defer func() {
+		metric.RequestTimingCounter.
+			With(prometheus.Labels{metric.TypeLabel: metric.ReadType, metric.OperationLabel: metric.GetLOperation}).
+			Observe(float64(time.Since(t).Milliseconds()))
+	}()
+
+	return s.storage.Get(id)
+
+}
+
+func (s *MetricStorage) Count() (int64, error) {
+	t := time.Now()
+	defer metric.RequestCounter.With(prometheus.Labels{metric.TypeLabel: metric.ReadType, metric.OperationLabel: metric.CountOperation}).Inc()
+	defer func() {
+		metric.RequestTimingCounter.
+			With(prometheus.Labels{metric.TypeLabel: metric.ReadType, metric.OperationLabel: metric.CountOperation}).
+			Observe(float64(time.Since(t).Milliseconds()))
+	}()
+
+	return s.storage.Count()
+
+}
